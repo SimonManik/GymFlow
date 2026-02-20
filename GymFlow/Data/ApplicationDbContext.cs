6b1f92c5fcf7c09cb1b220d4b368dbf40bf13205
Šimon-maník <xmanik@mendelu.cz>
2026-02-20 19:20:35 +0100

    Přidejte soubory projektu.

---
+++ GymFlow/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GymFlow.Models;
+
+namespace GymFlow.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) {}
+
+
+        public DbSet<Member> Members {  get; set; }
+
+
+
+    }
+}
