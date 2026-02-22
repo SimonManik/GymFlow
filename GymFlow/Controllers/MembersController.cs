5391e669f7eb64431ca6dabb50ff277721d28b0b
Šimon-maník <xmanik@mendelu.cz>
2026-02-22 21:26:22 +0100

    implement full edit functionality for members

---
+++ GymFlow/Controllers/MembersController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GymFlow.Data;
+using System.Linq;
+namespace GymFlow.Controllers
+{
+    public class MembersController:Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MembersController(ApplicationDbContext context) {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var members = _context.Members.ToList();
+
+            return View(members);
+        }
+
+        //Actions for capturing and storing data
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(GymFlow.Models.Member member)
+        {
+            _context.Members.Add(member);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var member = _context.Members.Find(id);
+
+            if (member != null)
+            {
+                _context.Members.Remove(member);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var member = _context.Members.Find(id);
+
+            if (member == null)
+            {
+                return NotFound();
+            }
+
+            return View(member);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, GymFlow.Models.Member member)
+        {
+            if (id != member.Id)
+            {
+                return NotFound();
+            }
+
+            _context.Members.Update(member);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+
