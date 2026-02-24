5b07f5ea5606f2846492c2d8434afc4e720865f6
Šimon-maník <xmanik@mendelu.cz>
2026-02-24 18:58:29 +0100

    add database context and statistics calculation to home controller

--- GymFlow/Controllers/HomeController.cs
+++ GymFlow/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
+using GymFlow.Data;
 using GymFlow.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace GymFlow.Controllers
 {
     public class HomeController : Controller
     {
+        // Database context variable
+        private readonly ApplicationDbContext _context;
+
+        // Constructor - injecting the database context
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            // LINQ queries to calculate statistics
+            ViewBag.TotalMembers = _context.Members.Count();
+            ViewBag.ActiveMembers = _context.Members.Count(m => m.MembershipExpiry >= DateTime.Now);
+            ViewBag.ExpiredMembers = _context.Members.Count(m => m.MembershipExpiry < DateTime.Now);
+
             return View();
         }
 
